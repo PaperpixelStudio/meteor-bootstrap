ab34f5fc1f2bb0b2f626211b0a922f88f083018c
drskullster <drskullster@gmail.com>
2015-02-06 16:17:39 +0100

    change package name

--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 Package.describe({
-  name: "PaperpixelStudio:bootstrap",
+  name: "ppxl:bootstrap",
   summary: "Highly configurable bootstrap integration.",
   version: "3.3.1_1",
   git: "https://github.com/Nemo64/meteor-bootstrap"
