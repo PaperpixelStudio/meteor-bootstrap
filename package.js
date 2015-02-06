a743db070a1f9c829ce972e85c5d01333f4fcdf4
Jonathan Berger <john@3kd.be>
2015-02-06 16:40:20 +0100

    change package.js

--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 Package.describe({
-  name: "ppxl:bootstrap",
+  name: "drskullster:bootstrap",
   summary: "Highly configurable bootstrap integration.",
   version: "3.3.1_1",
   git: "https://github.com/PaperpixelStudio/meteor-bootstrap"
